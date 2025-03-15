1ea0709e6bd8526a078979e1af8b3b3ebe14793e
Diego Ojeda <diegojeda775@yahoo.com>
2025-03-15 13:40:48 -0700

    add client profile

---
+++ src/app/profile/[username]/ProfilePageClient.tsx
@@ -0,0 +1,10 @@
+"use client"
+
+function ProfilePageClient(props: any) {
+  const { posts, likedPosts, isFollowing } = props
+  return (
+    <div>ProfilePageClient</div>
+  )
+}
+
+export default ProfilePageClient
--- src/app/profile/[username]/page.tsx
+++ src/app/profile/[username]/page.tsx
@@ -1,4 +1,6 @@
-import { getUserByUsername } from "@/app/actions/profile.action"
+import { getUserByUsername, getUserLikedPosts, getUserPosts, isFollowing } from "@/app/actions/profile.action"
+import { notFound } from "next/navigation";
+import ProfilePageClient from "./ProfilePageClient";
 
 export async function generateMetadata({params}: {params: {username: string}}) {
   const user = await getUserByUsername(params.username)
@@ -10,9 +12,19 @@ export async function generateMetadata({params}: {params: {username: string}}) {
   }
 }
 
-function ProfilePage({params}: {params: {username: string}}) {
+async function ProfilePage({params}: {params: {username: string}}) {
+  const user = await getUserByUsername(params.username);
+  if(!user) notFound();
+
+  const [posts, likedPosts, isCurrentUserFollowing] = await Promise.all([
+    getUserPosts(user.id),
+    getUserLikedPosts(user.id),
+    isFollowing(user.id),
+  ]);
+
+
   return (
-    <div>ProfilePage</div>
+   <ProfilePageClient posts={posts} likedPosts={likedPosts} isFollowing={isCurrentUserFollowing} />
   )
 }
 
